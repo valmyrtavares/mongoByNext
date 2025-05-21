724a13ace868e31c6a09d4c033d3b8bf4d91a26b
valmyrtavaes <valmyrtavares71@gmail.com>
2025-05-21 17:21:34 -0300

    delelte get

--- app/api/get/products/route.ts
+++ app/api/get/products/route.ts
@@ -1,26 +1,29 @@
-import { NextResponse } from 'next/server';
-import { MongoClient } from 'mongodb';
+// import { NextResponse } from 'next/server';
+// import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-  throw new Error('MONGODB_URI is not defined in environment variables');
-}
+// const uri = process.env.MONGODB_URI;
+// if (!uri) {
+//   throw new Error('MONGODB_URI is not defined in environment variables');
+// }
 
-const product = new MongoClient(uri);
-const dbName = 'next-db';
+// const product = new MongoClient(uri);
+// const dbName = 'next-db';
 
+// export async function GET() {
+//   try {
+//     await product.connect();
+//     const db = product.db(dbName);
+//     const collection = db.collection('product-next');
+//     const products = await collection.find({}).toArray();
+//     return NextResponse.json(products);
+//   } catch (error) {
+//     console.error('Erro ao buscar produtos:', error);
+//     return NextResponse.json(
+//       { error: 'Erro ao buscar usuários' },
+//       { status: 500 }
+//     );
+//   }
+// }
 export async function GET() {
-  try {
-    await product.connect();
-    const db = product.db(dbName);
-    const collection = db.collection('product-next');
-    const products = await collection.find({}).toArray();
-    return NextResponse.json(products);
-  } catch (error) {
-    console.error('Erro ao buscar produtos:', error);
-    return NextResponse.json(
-      { error: 'Erro ao buscar usuários' },
-      { status: 500 }
-    );
-  }
+  return new Response('ok');
 }
